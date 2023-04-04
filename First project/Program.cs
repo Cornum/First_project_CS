35e0fe0dad58aadd674d6f7919a63e122773ec24
Cornum <cornumKisara@gmail.com>
2023-04-04 15:20:54 +0200

    While -> For

--- First project/Program.cs
+++ First project/Program.cs
@@ -12,69 +12,29 @@ namespace First_project
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
-            string password = "123qwe";
-            Console.WriteLine("Введите пароль от ячейки");
-            string userInput = Console.ReadLine();
+            int age;
+            Console.Write("Введите ваш возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
 
-            if (userInput == password)
-            {
-                Console.WriteLine("Пароль принят. Доступ разрешен");
-            }
-            else
-            {
-                Console.WriteLine("Неверный пароль. Полиция была вызвана.");
-                Console.SetWindowSize(800, 600);
-            }
-            float rublesInWallet, dollarsInWallet;
-            int rubToUsd = 64, usdToRub = 66;
-            float exchangeCurrencyCount;
-            
-            Console.WriteLine("Добро пожаловать в обменник валют!");
-            Console.Write("Введите баланс рублей: ");
-            rublesInWallet = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Введите баланс долларов: ");
-            dollarsInWallet = Convert.ToSingle(Console.ReadLine());
 
-            Console.WriteLine("Выберите необходимую операцию.");
-            Console.WriteLine("1 - обменять рубли на доллары.");
-            Console.WriteLine("2 - обменять доллары на рубли.");
-            Console.Write("Ваш выбор: ");
-            string desiredOperation = Console.ReadLine();
-            switch (desiredOperation)
+            while (age-->0)
             {
-                case "1":
-                    Console.WriteLine("Обмен рублей на доллары.");
-                    Console.WriteLine("Сколько вы хотите поменять?");
-                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (rublesInWallet >= exchangeCurrencyCount)
-                    {
-                        rublesInWallet -= exchangeCurrencyCount;
-                        dollarsInWallet += exchangeCurrencyCount / rubToUsd;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Недопустимое количество рублей.");
-                    }
-                    break;
-                case "2":
-                    Console.WriteLine("Обмен долларов на рубли.");
-                    Console.WriteLine("Сколько вы хотите поменять?");
-                    exchangeCurrencyCount = Convert.ToSingle(Console.ReadLine());
-                    if (dollarsInWallet >= exchangeCurrencyCount)
-                    {
-                        dollarsInWallet -= exchangeCurrencyCount;
-                        rublesInWallet += exchangeCurrencyCount * usdToRub;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Недопустимое количество долларов.");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Неверно введены данные.");
-                    break;
+                if (age == 10)
+                {
+                    continue;
+                }
+                Console.WriteLine(age);
             }
-            Console.WriteLine($"Ваш баланс: {rublesInWallet} рублей и " + $"{dollarsInWallet} долларов.");
+
+            for (int i=0; i <= 100; i+=2)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
