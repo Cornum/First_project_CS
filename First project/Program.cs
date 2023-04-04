0a42222eae95ec2aa2406cdca42c0296870e6423
Cornum <cornumKisara@gmail.com>
2023-04-04 16:04:12 +0200

    GAME Угадай число

--- First project/Program.cs
+++ First project/Program.cs
@@ -10,21 +10,38 @@ namespace First_project
     {
         static void Main(string[] args)
         {
-            /*Random rand = new Random();
-            int value;
-
-            while (true)
-            {
-                value = rand.Next(0, 10);
-                Console.WriteLine(value);
-                Console.ReadKey();
-            }*/
             Random rand = new Random();
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine(rand.Next(0, 5));
-            }
+            int number;
+            int lower, higher;
+            int triesCount = 5;
+            int userInput;
 
+            number = rand.Next(0, 101);
+            lower = rand.Next(number - 10, number);
+            higher = rand.Next(number + 1, number + 10);
+
+            Console.WriteLine($"Мы загадали число от 0 до 100. Оно больше чем {lower}, " +
+                $"но меньше чем  {higher}.");
+            Console.WriteLine($"Что это за число? У вас {triesCount} попыток отгадать.");
+
+            while (triesCount-- > 0)
+            {
+                Console.Write("Ваш ответ: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == number)
+                {
+                    Console.WriteLine("Вы правы, это было " + number + ".");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Неверно. Попробуйте ещё раз.");
+                }
+            }
+            if (triesCount < 0)
+            {
+                Console.WriteLine("Вы проиграли. Это было число " + number + ".");
+            }
         }
     }
 }
