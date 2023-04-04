4a3d0d3dce62914fa287e3b326d213eca682ef1e
Cornum <cornumKisara@gmail.com>
2023-04-04 15:40:49 +0200

    Practical cycles

--- First project/Program.cs
+++ First project/Program.cs
@@ -12,29 +12,67 @@ namespace First_project
         {
             Console.OutputEncoding = Encoding.Unicode;
             Console.InputEncoding = Encoding.Unicode;
-            int age;
-            Console.Write("Введите ваш возраст: ");
-            age = Convert.ToInt32(Console.ReadLine());
 
+            /*int triesCount = 5;
+            string password = "123qwe";
+            string userInput;
 
-            while (age-->0)
+            for(int i=0; i<triesCount; i++)
             {
-                if (age == 10)
+                Console.WriteLine("Введите пароль: ");
+                userInput = Console.ReadLine(); 
+                if(userInput == password)
                 {
-                    continue;
+                    Console.WriteLine("Секретные данные.");
+                    break;
                 }
-                Console.WriteLine(age);
+                else
+                {
+                    Console.WriteLine("Неверный пароль.");
+                    Console.WriteLine($"У вас осталось {triesCount-(i+1)} попыток");
+
+                }
+            }*/
+            /*float money;
+            int years, percent;
+
+            Console.Write("Введите количество денег, внесенных на вклад: ");
+            money = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("На сколько лет открыт вклад?");
+            years = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Под какой процент?");
+            percent = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < years; i++)
+            {
+                money += money * percent / 100;
+                Console.WriteLine($"В {i+1} году у вас {money} рублей.");
+                Console.ReadKey();
+            }*/
+            int playerHealth = 1000, playerDamage = 5;
+            int enemyHealth = 1000, enemyDamage = 5;
+
+            while (playerHealth > 0 && enemyHealth > 0)
+            {
+                playerHealth -= enemyDamage;
+                enemyHealth -= playerDamage;
+
+                Console.WriteLine(playerHealth + " - игрок.");
+                Console.WriteLine(enemyHealth + " - враг.");
             }
 
-            for (int i=0; i <= 100; i+=2)
+            if(playerHealth <= 0 && enemyHealth <= 0)
             {
-                if (i == 0)
-                {
-                    continue;
-                }
-                Console.WriteLine(i);
+                Console.WriteLine("Ничья.");
             }
-
+            else if (enemyHealth <= 0)
+            {
+                Console.WriteLine("Победа игрока.");
+            }
+            else if (playerHealth <= 0)
+            {
+                Console.WriteLine("Победа врага.");
+            }
         }
     }
 }
