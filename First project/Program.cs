8cf172e2a3103d21de581d37787961254cdbb6d2
Cornum <cornumKisara@gmail.com>
2023-04-06 00:31:38 +0200

    Ref Out in Function

--- First project/Program.cs
+++ First project/Program.cs
@@ -10,20 +10,47 @@ namespace First_project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello!");
-            Console.WriteLine("Are you here?");
-            WriteError("No signal");
-            Console.WriteLine("Strange...");
-            WriteError("Inthernet wasn't paid.", ConsoleColor.Yellow);
-            WriteError("Pay first.", symbol: '@');
+            int sum = 0, x = 1, y = 5;
+            //Отправляет значения в переменных
+            Add(sum, x, y);
+            //Отправляет ссылку на место Sum в памяти. Должны сначала инициализировать
+            Add2(ref sum, x, y);
+            //Отправляет ссылку на место Sum в памяти. Инициализирует память сама.
+            int sum2;
+            Add3(out sum2, x, y);
+            Console.WriteLine(sum);
+            Console.WriteLine(sum2);
+
+            int[] array = new int[5];
+            int[] array2 = new int[5];
+            EditArray(array, 2, 5);
+            Console.WriteLine(array[2]);
+            array2 = EditArrayR(array, 2, 5);
+            Console.WriteLine(array2[2]);
         }
 
-        static void WriteError(string text, ConsoleColor color = ConsoleColor.Red, char symbol = '!') //Необязательный всегда в конце параметров
+        static void Add(int sum, int x, int y)
         {
-            ConsoleColor defaultColor = Console.ForegroundColor;
-            Console.ForegroundColor = color;
-            Console.WriteLine(symbol + text);
-            Console.ForegroundColor = defaultColor;
+            sum = x + y;
+        }
+        static void Add2(ref int sum, int x, int y)
+        {
+            sum = x + y;
+        }
+        static void Add3(out int sum, int x, int y)
+        {
+            sum = x + y;
+        }
+
+        static void EditArray(int[] array, int index, int value)
+        {
+            array[index] = value;
+        }
+        static int[] EditArrayR(int[] array, int index, int value)
+        {
+            array = new int[6];
+            array[index] = value;
+            return array;
         }
     }
 }
