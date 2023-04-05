89b1fbecaf577ef8e260a51b24be6dbb09cc05ce
Cornum <cornumKisara@gmail.com>
2023-04-05 23:55:56 +0200

    Game Treasure hunter

--- First project/Program.cs
+++ First project/Program.cs
@@ -10,22 +10,110 @@ namespace First_project
     {
         static void Main(string[] args)
         {
-            int[] bag = new int[1];
-            bag[0] = 12;
-            int[] tempBag = new int[bag.Length + 1];
-            for (int i = 0; i < bag.Length; i++)
+            int treasureCount = 0;
+            bool isOpen = true;
+            Console.CursorVisible = false;
+            char[,] map = {
+                { '#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+                { '#','X','#',' ',' ',' ','X',' ',' ',' ',' ',' ',' ',' ',' ','X','#'},
+                { '#',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ','#',' ',' ',' ',' ',' ',' ',' ','#','#','#','#',' ',' ','#'},
+                { '#',' ','#',' ',' ',' ',' ',' ',' ',' ','#',' ','X','#',' ',' ','#'},
+                { '#',' ',' ',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#',' ',' ','#'},
+                { '#',' ','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#',' ',' ','#'},
+                { '#',' ','#',' ',' ',' ',' ',' ',' ',' ','#',' ',' ','#',' ',' ','#'},
+                { '#',' ','#','#','#','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ','X',' ','#',' ',' ','X',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+                { '#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ',' ','X','#'},
+                { '#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'}
+            };
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                tempBag[i] = bag[i];
-                Console.WriteLine(bag[i] + " first");
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == 'X')
+                    {
+                        treasureCount++;
+                    }
+                }
             }
-            Console.WriteLine();
-            tempBag[tempBag.Length - 1] = 56;
 
-            bag = tempBag;
-            for (int i = 0; i < bag.Length; i++)
+            int userX = 6, userY = 6;
+            char[] bag = new char[1];
+
+            while (isOpen)
             {
-                Console.WriteLine(bag[i] + " second");
+                Console.SetCursorPosition(0, 20);
+                Console.Write("Bag: ");
+                for (int i = 0; i < bag.Length; i++)
+                {
+                    Console.Write(bag[i] + " ");
+                }
+
+                Console.SetCursorPosition(0, 0);
+                for (int i = 0; i < map.GetLength(0); i++)
+                {
+                    for (int j = 0; j < map.GetLength(1); j++)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    Console.WriteLine();
+                }
+                Console.SetCursorPosition(userY, userX);
+                Console.Write('@');
+                ConsoleKeyInfo charKey = Console.ReadKey();
+                switch (charKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (map[userX - 1, userY] != '#')
+                        {
+                            userX--;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (map[userX + 1, userY] != '#')
+                        {
+                            userX++;
+                        }
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (map[userX, userY - 1] != '#')
+                        {
+                            userY--;
+                        }
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (map[userX, userY + 1] != '#')
+                        {
+                            userY++;
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        isOpen = false;
+                        break;
+                }
+
+                if (map[userX, userY] == 'X')
+                {
+                    map[userX, userY] = 'o';
+                    char[] tempBag = new char[bag.Length + 1];
+                    for (int i = 0; i < bag.Length; i++)
+                    {
+                        tempBag[i] = bag[i];
+                    }
+                    tempBag[tempBag.Length - 1] = 'X';
+                    bag = tempBag;
+                    treasureCount--;
+                }
+                if (treasureCount == 0)
+                {
+                    isOpen = false;
+                }
+                Console.Clear();
+                Console.ReadKey();
             }
+            Console.WriteLine("Thank you for playing!");
         }
     }
 }
