aa061fbae564250b24f75dcf99a24f337eb40f8f
Cornum <cornumKisara@gmail.com>
2023-04-05 22:25:11 +0200

    Practical Многомерные массивы

--- First project/Program.cs
+++ First project/Program.cs
@@ -10,37 +10,76 @@ namespace First_project
     {
         static void Main(string[] args)
         {
-            /*int[,] array;
-            int[,] array2 = new int[2, 3];
-
-            int[,] array3 = {
-                { 2, 3, 4 },
-                { 4, 5, 1 },
-                { 7, 8, 9 }
+            string[,] books = {
+                { "Pushkin A.","Lermontov M.","Esenin S."},
+                { "Martin R.","Shell J.","Teplyakov S."},
+                { "King S.","Lovecraft H.","Stocker B."}
             };
-
-            int[,] array4 = new int[2, 3] {
-                { 9,8,7},
-                { 6,5,4}
-            };
-
-            Console.WriteLine(array3[2, 2]);
-            Console.WriteLine(array3.Length);*/
-
-            int[,] array3 = new int[15,25];
-            Random rand = new Random();
-
-
-            for (int i = 0; i < array3.GetLength(0); i++)
+            bool isOpen = true;
+            while (isOpen)
             {
-                for (int j = 0; j < array3.GetLength(1); j++)
+                Console.SetCursorPosition(0, 20);
+                Console.WriteLine("Array of all authors:\n\n");
+                for (int i = 0; i < books.GetLength(0); i++)
                 {
-                    array3[i, j] = rand.Next(0, 10);
-                    Console.Write(array3[i, j] + " ");
+                    for (int j = 0; j < books.GetLength(1); j++)
+                    {
+                        Console.Write(books[i, j] + " | ");
+                    }
+                    Console.WriteLine();
                 }
-                Console.WriteLine();
-            }
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Library");
+                Console.WriteLine("\n\n1 - find author by index.\n2 - find book by author.\n3 - exit.");
+                Console.Write("Make a choise: ");
 
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        int line, column;
+                        Console.Write("Enter line number: ");
+                        line = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Enter column number: ");
+                        column = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine($"This author is {books[line - 1, column - 1]}");
+                        break;
+                    case 2:
+                        Console.Write("Enter the author: ");
+                        bool authorIsFound = false;
+                        string author = Console.ReadLine();
+                        for (int i = 0; i < books.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < books.GetLength(1); j++)
+                            {
+                                if (author.ToLower() == books[i, j].ToLower())
+                                {
+                                    Console.WriteLine($"Author {books[i, j]} is on line {i + 1}, " +
+                                        $"column {j + 1}");
+                                    authorIsFound = true;
+                                }
+                            }
+                        }
+                        if (authorIsFound == false)
+                        {
+                            Console.WriteLine($"Author {author} isn't found.");
+                        }
+                        break;
+                    case 3:
+                        isOpen = false;
+                        Console.WriteLine("Bye bye!");
+                        break;
+                    default:
+                        Console.WriteLine("Error! Unknown order!");
+                        break;
+                }
+
+                if (isOpen)
+                {
+                    Console.WriteLine("\nPress any buton...");
+                }
+                Console.ReadKey();
+                Console.Clear();
+            }
         }
     }
 }
