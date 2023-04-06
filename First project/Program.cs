d4faba8ab787a921c75632c1eb91c61402c777e1
Cornum <cornumKisara@gmail.com>
2023-04-06 14:55:30 +0200

    FIFO Queue

--- First project/Program.cs
+++ First project/Program.cs
@@ -10,27 +10,19 @@ namespace First_project
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
-            numbers.Add(12);
-            numbers.Add(5);
-            numbers.Add(8);
-            numbers.Add(14);
-            numbers.Add(22);
+            Queue<string> patients = new Queue<string>();
 
-            numbers.AddRange(new int[] { 3, 8, 4, 1 });
+            patients.Enqueue("Vasiliy");
+            patients.Enqueue("Alexey");
+            patients.Enqueue("Roman");
+            patients.Enqueue("Vladimir");
 
-            numbers.RemoveAt(3);
+            Console.WriteLine("Here is " + patients.Dequeue());
 
-            numbers.Remove(5);
-
-            numbers.Insert(1, 123);
-
-            Console.WriteLine("First apearence of number 8 is " + numbers.IndexOf(8));
-            //numbers.Clear();
-
-            for (int i = 0; i < numbers.Count; i++)
+            Console.WriteLine("Next will be " + patients.Peek());
+            foreach (var patien in patients)
             {
-                Console.WriteLine(numbers[i]);
+                Console.WriteLine(patien);
             }
         }
     }
